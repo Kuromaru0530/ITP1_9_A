a83cdb4d353aebdb4b14ff1a2b0ce79b6d79ddc3
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:31:03 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP1_9_A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var w = Console.ReadLine();
+        List<string> list = new List<string>();
+        while (true)
+        {
+            string Input = Console.ReadLine();
+            if (Input == "END_OF_TEXT") break;
+            else list.Add(Input.ToLower());
+        }
+        string T1 = string.Join(" ", list);
+        string[] T2 = T1.Split(' ');
+        int ans = 0;
+        for(int i = 0; i < T2.Length; i++)
+        { 
+            if(T2[i] == w)
+            {
+                ans++;
+            }
+        }
+        Console.WriteLine(ans);
+    }
+}
